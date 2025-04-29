d478ab3132711f391bd0c353ea6c9d0a9bc77ad7
Dmytro Lobanov <dspwww@gmail.com>
2025-04-29 12:53:32 +0100

    feat: Add general & education section
    Add minimal structures for general & education sections and handle to their updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,98 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [person, setPerson] = useState({
+    name: '',
+    age: '',
+    email: '',
+  });
+
+  const [education, setEducation] = useState({
+    schoolname: '',
+    titleofstudy: '',
+    studystart: '',
+    studyend: '',
+  });
+
+  const handlePersonUpdate = (event, prop) => {
+    const { value } = event.target;
+    setPerson({
+      ...person,
+      [prop]: value,
+    })
+  }
+
+  const handleEducationUpdate = (event, prop) => {
+    const { value } = event.target;
+    setEducation({
+      ...education,
+      [prop]: value,
+    })
+  }
+
 
   return (
     <>
+      <h1>CV Application</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>General information</h2>
+        <div>
+          <label htmlFor="name">Full name</label>
+          <input 
+            type="text" name="name" id="name" 
+            value={person.name} onChange={(event) => handlePersonUpdate(event, 'name')} />
+        </div>
+
+        <div>
+          <label htmlFor="age">Age</label>
+          <input 
+            type="number" name="age" id="age"
+            value={person.age} onChange={(event) => handlePersonUpdate(event, 'age')} />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+            type="email" name="email" id="email" 
+            value={person.email} onChange={(event) => handlePersonUpdate(event, 'email')} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h2>
+          Educational experience
+        </h2>
+
+        <div>
+          <div>
+            <label htmlFor="schoolname">School Name</label>
+            <input 
+              type="text" name="schoolname" id="schoolname" 
+              value={person.schoolname} onChange={(event) => handleEducationUpdate(event, 'schoolname')} />
+          </div>
+
+          <div>
+            <label htmlFor="titleofstudy">Title of study</label>
+            <input 
+              type="text" name="titleofstudy" id="titleofstudy" 
+              value={person.titleofstudy} onChange={(event) => handleEducationUpdate(event, 'titleofstudy')} />
+          </div>
+
+          <div>
+            <label htmlFor="studystart">Dates of studing</label>
+            <input 
+              type="date" name="studystart" id="studystart"
+              value={person.studystart} onChange={(event) => handleEducationUpdate(event, 'studystart')} />
+            <input 
+              type="date" name="studyend" id="studyend"
+              value={person.studyend} onChange={(event) => handleEducationUpdate(event, 'studyend')} />
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
