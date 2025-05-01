d130c118476add4a4c20988afd8ef5d217e97bac
Dmytro Lobanov <dspwww@gmail.com>
2025-05-01 18:20:47 +0100

    feat: Add GeneralInformationForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 import Input from './components/Input';
+import GeneralInformationForm from './components/GeneralInformation';
 
 function App() {
   const [person, setPerson] = useState({
@@ -19,14 +18,6 @@ function App() {
     studyend: '',
   });
 
-  const handlePersonUpdate = (event, prop) => {
-    const { value } = event.target;
-    setPerson({
-      ...person,
-      [prop]: value,
-    })
-  }
-
   const handleEducationUpdate = (event, prop) => {
     const { value } = event.target;
     setEducation({
@@ -38,32 +29,26 @@ function App() {
 
   return (
     <div className="min-h-full bg-gray-100">
-      <header class="bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">CV Application</h1>
+      <header className="bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">CV Application</h1>
         </div>
       </header>
 
       <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <div className="max-w-sm p-6 mb-6 bg-white border border-gray-200 rounded-lg shadow-sm border-b border-gray-900/10 pb-12">
-            <h2 class="text-base/7 font-semibold text-gray-900">General information</h2>
-            <Input name="name" label="Full name" value={person.name} onChangeHandler={handlePersonUpdate} />
-            <Input type="number" name="age" label="Age" value={person.age} onChangeHandler={handlePersonUpdate} />
-            <Input type="email" name="email" label="Email" value={person.email} onChangeHandler={handlePersonUpdate} />
-          </div>
+        <div className="mx-auto max-w-7xl px-4 py-6">
+          <GeneralInformationForm personInfo={person} onUpdate={setPerson} />
 
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm border-b border-gray-900/10 pb-12">
-            <h2 class="text-base/7 font-semibold text-gray-900">Educational experience</h2>
-            <Input name="schoolname" label="School Name" value={education.schoolname} onChangeHandler={handleEducationUpdate} />
+            <h2 className="text-base/7 font-semibold text-gray-900">Educational experience</h2>
 
+            <Input name="schoolname" label="School Name" value={education.schoolname} onChangeHandler={handleEducationUpdate} />
             <Input name="titleofstudy" label="Title of study" value={education.titleofstudy} onChangeHandler={handleEducationUpdate} />
-            
             <Input type="date" name="studystart" label="Start date of study" value={education.studystart} onChangeHandler={handleEducationUpdate} />
             <Input type="date" name="studyend" label="End date of study" value={education.studyend} onChangeHandler={handleEducationUpdate} />
 
+            <button type="button" className="py-2.5 px-5 me-2 mb-2 mt-7 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Add more education</button>
           </div>
-
         </div>
       </main>
     </div>
---
+++ src/components/GeneralInformation.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Input from "./Input";
+
+const GeneralInformationForm = ({personInfo, onUpdate}) => {
+  const [person, setPerson] = useState(personInfo);
+
+  const btnStyles = 'py-2.5 px-5 me-2 mb-2 mt-7 text-sm font-medium text-gray-900 cursor-pointer focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100';
+
+  const handlePersonUpdate = (event, prop) => {
+    const { value } = event.target;
+
+    setPerson({
+      ...person,
+      [prop]: value,
+    });
+  }
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    onUpdate(person);
+  }
+
+  return (
+    <div className="max-w-sm p-6 mb-6 bg-white border border-gray-200 rounded-lg shadow-sm border-b border-gray-900/10 pb-12">
+      <h2 className="text-base/7 font-semibold text-gray-900">General information</h2>
+      <Input name="name" label="Full name" value={person.name} onChangeHandler={handlePersonUpdate} />
+      <Input type="number" name="age" label="Age" value={person.age} onChangeHandler={handlePersonUpdate} />
+      <Input type="email" name="email" label="Email" value={person.email} onChangeHandler={handlePersonUpdate} />
+      <button type="button" className={btnStyles} onClick={handleUpdate}>Update</button>
+    </div>
+  )
+}
+
+export default GeneralInformationForm;
